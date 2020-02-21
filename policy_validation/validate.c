5ba4b43df58ec001c4a5970877e077b9893dade4
Djordje Golubovic <djordje.golubovic@nttdata.ro>
2020-02-21 17:31:18 +0100

    Added command line tool for policy file verification.
    Added policy file format checking.

--- policy_validation/validate.c
+++ policy_validation/validate.c
@@ -12,15 +12,20 @@ static int policy_data_len = 0;
 int main(int argc, char** argv) {
     FILE *fp;
 
-    fp = fopen("policy.json", "r");
+    if (argc != 2) {
+        printf("Input file required!\n");
+        return 1;
+    }
+
+    fp = fopen(argv[1], "r");
 
     if (fp == NULL) {
-        printf("No such file: policy.json\n");
+        printf("No such file: %s\n", argv[1]);
         exit(1);
     }
 
     while (!feof(fp)) {
-        int read_bytes = fread(&policy_data[policy_data_len], 512, 1, fp);
+        int read_bytes = fread(&policy_data[policy_data_len], 1, 512, fp);
         policy_data_len += read_bytes;
         if (policy_data_len >= 4096) break;
     }
@@ -28,6 +33,12 @@ int main(int argc, char** argv) {
     fclose(fp);
     Validator_report_t report;
     Validator_check(policy_data, &report);
+
+#define YESNO(condition) (condition) ? "yes" : "no"
+
+    printf("validator report:\n - valid json: %s\n - proper format: %s\n",
+        YESNO(report.valid_json == 1),
+        YESNO(report.proper_format == 1));
 
     return 0;
 }
