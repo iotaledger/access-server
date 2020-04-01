aa216e6fe1e5f2da4809f704b00392280040e534
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2020-04-01 11:55:13 +0200

    rm pokw dir https://github.com/iotaledger/access/issues/30

--- pokw/policy-server/bc_deploy_contract.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -f docker-compose-bc-deploy.yml build bc-ps-deployer
-docker-compose -f docker-compose-bc-deploy.yml run bc-ps-deployer
-docker-compose -f docker-compose-bc-deploy.yml rm -f
--- pokw/vehicle-data-webui/bc_stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose -p vd-bc-webui -f docker-compose-bc.yml down
