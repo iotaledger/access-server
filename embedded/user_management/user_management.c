8aa88b2d922aac0535062db193167ec83b2173da
strahinjagolic <golic.s90@gmail.com>
2020-04-27 22:29:53 +0200

    Code cleanup for User Management
    - Remove magic numbers and replace them for macros
    + Aligne coding style through whole file.

--- embedded/user_management/user_management.c
+++ embedded/user_management/user_management.c
@@ -19,8 +19,6 @@
 
 #include "user_management.h"
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include <sqlite3.h>
@@ -30,8 +28,18 @@
 
 #include "jsmn.h"
 
+#define QUERY_STR_LEN 1024
+#define QUERY_STR_MAX 2048
+#define BC_HOSTNAME_LEN 256
+#define DEVICE_ID_LEN 67
+#define MSG_LEN 256
+#define RESPONSE_LEN 1024*1024
+#define STR_SIZE 128
+#define PUBLIC_ID_SIZE 256
+#define URL_LEN 1024
+
 static sqlite3* users_db;
-static const char fixed_users_query_strings[][1024] = {
+static const char fixed_users_query_strings[][QUERY_STR_LEN] = {
     "INSERT INTO users (username, firstName, lastName, walletId, publicId) " \
     "VALUES ('jamie', 'Jamie', 'Owner', '0x9A61Ad437E90022946A72abBa55fea3FE7a42dd'," \
     "'3c9d985c5d630e6e02f676997c5e9f03b45c6b7529b2491e8de03c18af3c9d87f0a65ecb5dd8f390dee13835354b222df414104684ce9f1079a059f052ca6e51');",
@@ -51,36 +59,42 @@ static const char fixed_users_query_strings[][1024] = {
 
 
 static CURL* curl = 0;
-static char bc_hostname[256] = "";
+static char bc_hostname[BC_HOSTNAME_LEN] = "";
 static int bc_port = 0;
-static char device_id[67] = "";
+static char device_id[DEVICE_ID_LEN] = "";
 
 
 #define USER_MANAGEMENT_INSERT_QUERY "INSERT INTO users (username, firstName, lastName, walletId, publicId, userId) VALUES ('%s', '%s', '%s', '%s', '%s', '%s');"
 
 static int count_cb(void *NotUsed, int argc, char **argv, char **azColName) {
-    int i;
-    for(i = 0; i<argc; i++) {
-        //printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    int i = argc;
+
+    if (NotUsed != NULL) {
+        *((int*)NotUsed) = i;
     }
-    if (NotUsed != NULL) *((int*)NotUsed) = i;
     return 0;
 }
 
 static int get_all_cb(void *data, int argc, char **argv, char **azColName) {
-    if (data == NULL) return -1;
+    if (data == NULL) {
+        return -1;
+    }
     fjson_object* fjobj = (fjson_object*)data;
     fjson_object* fj_new_el = fjson_object_new_object();
     fjson_object_array_add(fjobj, fj_new_el);
     for (int i = 0; i<argc; i++) {
-        if (argv[i] == NULL) continue;
+        if (argv[i] == NULL) {
+            continue;
+        }
         fjson_object_object_add(fj_new_el, azColName[i], fjson_object_new_string(argv[i]));
     }
     return 0;
 }
 
 static int get_cb(void *data, int argc, char **argv, char **azColName) {
-    if (data == NULL) return -1;
+    if (data == NULL) {
+        return -1;
+    }
     fjson_object* fjobj = (fjson_object*)data;
     for (int i = 0; i<argc; i++) {
         fjson_object_object_add(fjobj, azColName[i], fjson_object_new_string(argv[i]));
@@ -94,7 +108,7 @@ static int table_exists() {
     char* errMsg;
     int rc = sqlite3_exec(users_db, "SELECT name FROM sqlite_master WHERE type='table' AND name='users';", count_cb, &check, &errMsg);
 
-    if(rc != SQLITE_OK){
+    if(rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
         return -1;
@@ -103,13 +117,12 @@ static int table_exists() {
     return check;
 }
 
-static int query_helper(const char* query, int (*cb)(void*,int,char**,char**), void* data)
-{
+static int query_helper(const char* query, int (*cb)(void*,int,char**,char**), void* data) {
     char* errMsg;
     int rc;
     rc = sqlite3_exec(users_db, query, cb, data, &errMsg);
 
-    if(rc != SQLITE_OK){
+    if(rc != SQLITE_OK) {
         fprintf(stderr, "SQL error: %s\n", errMsg);
         sqlite3_free(errMsg);
         return -1;
@@ -118,21 +131,24 @@ static int query_helper(const char* query, int (*cb)(void*,int,char**,char**), v
     return 0;
 }
 
-static void create_json_response(fjson_object* response, fjson_object* data, int err, char* msg)
-{
-    if (data != NULL) fjson_object_object_add(response, "data", data);
+static void create_json_response(fjson_object* response, fjson_object* data, int err, char* msg) {
+    if (data != NULL) {
+        fjson_object_object_add(response, "data", data);
+    }
 
-    if (msg != NULL) fjson_object_object_add(response, "message", fjson_object_new_string(msg));
-    else fjson_object_object_add(response, "message", fjson_object_new_string(""));
+    if (msg != NULL) {
+        fjson_object_object_add(response, "message", fjson_object_new_string(msg));
+    } else {
+        fjson_object_object_add(response, "message", fjson_object_new_string(""));
+    }
 
     fjson_object_object_add(response, "error", fjson_object_new_int(err));
 }
 
-static char query_string[2048];
+static char query_string[QUERY_STR_MAX];
 
-static int insert_user(const char* username, const char* firstName, const char* lastName, const char* walletId, const char* publicId, const char* userId)
-{
-    snprintf(query_string, 2048, USER_MANAGEMENT_INSERT_QUERY, username, firstName, lastName, walletId, publicId, userId);
+static int insert_user(const char* username, const char* firstName, const char* lastName, const char* walletId, const char* publicId, const char* userId) {
+    snprintf(query_string, QUERY_STR_MAX, USER_MANAGEMENT_INSERT_QUERY, username, firstName, lastName, walletId, publicId, userId);
 
     return query_helper(query_string, count_cb, NULL);
 }
@@ -141,7 +157,7 @@ static int init_table() {
     int rc;
     rc = query_helper("CREATE TABLE users (username TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, walletId TEXT, publicId TEXT, userId TEXT);", count_cb, NULL);
 
-    if(rc != 0){
+    if(rc != 0) {
         return -1;
     }
 
@@ -155,25 +171,28 @@ static int init_table() {
     return 0;
 }
 
-int UserManagement_init(const char* bc_hostname_, int bc_port_, const char* device_id_)
-{
+int UserManagement_init(const char* bc_hostname_, int bc_port_, const char* device_id_) {
     CURLcode curl_status = curl_global_init(CURL_GLOBAL_ALL);
 
-    if (bc_port_ > 0) bc_port = bc_port_;
-    if (bc_hostname != NULL) {
-        strncpy(bc_hostname, bc_hostname_, 256);
+    if (bc_port_ > 0) {
+        bc_port = bc_port_;
     }
-    if (device_id_ != NULL) strncpy(device_id, device_id_, sizeof(device_id));
 
-    if (curl_status != 0)
-    {
+    if (bc_hostname != NULL) {
+        strncpy(bc_hostname, bc_hostname_, BC_HOSTNAME_LEN);
+    }
+
+    if (device_id_ != NULL) {
+        strncpy(device_id, device_id_, sizeof(device_id));
+    }
+
+    if (curl_status != 0) {
         fprintf(stderr, "curl_global_init failed!");
         exit(-1);
     }
 
     curl = curl_easy_init();
-    if (curl == NULL)
-    {
+    if (curl == NULL) {
         fprintf(stderr, "curl_easy_init failed!");
         exit(-1);
     }
@@ -189,12 +208,9 @@ int UserManagement_init(const char* bc_hostname_, int bc_port_, const char* devi
 
     int check = table_exists();
     // does table 'users' exist?
-    if (check < 0)
-    {
+    if (check < 0) {
         return rc-1;
-    }
-    else if (check == 0)
-    {
+    } else if (check == 0) {
         rc = init_table();
         if (rc < 0) {
             return rc-2;
@@ -224,42 +240,43 @@ void UserManagement_get_all_users(char* response)
         create_json_response(fjobj_resp, NULL, 1, "Couldn't get all users!");
     }
 
-    strncpy(response, fjson_object_to_json_string(fjobj_resp), 2048);
+    strncpy(response, fjson_object_to_json_string(fjobj_resp), QUERY_STR_MAX);
 }
 
-void UserManagement_get_string(const char* id, char* response)
-{
+void UserManagement_get_string(const char* id, char* response) {
     fjson_object* fjobj_response = fjson_object_new_object();
-    snprintf(query_string, 2048, "SELECT * FROM users WHERE username = '%s'", id);
+    snprintf(query_string, QUERY_STR_MAX, "SELECT * FROM users WHERE username = '%s'", id);
     int rc = query_helper(query_string, get_cb, fjobj_response);
 
     fjson_object* fjobj_resp = fjson_object_new_object();
     if (rc == 0) {
         create_json_response(fjobj_resp, fjobj_response, 0, "");
     } else {
-        char msg[256] = "";
-        snprintf(msg, 256, "Couldn't get '%s'!", id);
+        char msg[MSG_LEN] = "";
+        snprintf(msg, MSG_LEN, "Couldn't get '%s'!", id);
         create_json_response(fjobj_resp, NULL, 1, msg);
     }
 
-    strncpy(response, fjson_object_to_json_string(fjobj_resp), 2048);
+    strncpy(response, fjson_object_to_json_string(fjobj_resp), QUERY_STR_MAX);
 }
 
 #define JSMN_TOK_MAX 128
 static jsmn_parser p;
 static jsmntok_t t[JSMN_TOK_MAX];
 
-static char response[1024*1024];
+static char response[RESPONSE_LEN];
 
-static size_t wallet_id_callback(void *buffer, size_t size, size_t nmemb, void *userp)
-{
+static size_t wallet_id_callback(void *buffer, size_t size, size_t nmemb, void *userp) {
     char* in_data = (char*)buffer;
     char* out_data = (char*)userp;
     jsmn_init(&p);
     size_t r = jsmn_parse(&p, in_data, nmemb, t, JSMN_TOK_MAX);
-    for (int i = 0; i < r; i++){
-        if (t[i].size == 0) continue;
-        if (strncmp(&in_data[t[i].start], "address", 7) == 0) {
+    for (int i = 0; i < r; i++) {
+        if (t[i].size == 0) {
+            continue;
+        }
+
+        if (strncmp(&in_data[t[i].start], "address", strlen("address")) == 0) {
             strncpy(out_data, &in_data[t[i+1].start], t[i+1].end - t[i+1].start);
             break;
         }
@@ -267,34 +284,35 @@ static size_t wallet_id_callback(void *buffer, size_t size, size_t nmemb, void *
     return nmemb;
 }
 
-void UserManagement_put_string(const char* json_string, char* json_response)
-{
+void UserManagement_put_string(const char* json_string, char* json_response) {
     jsmn_init(&p);
     size_t r = jsmn_parse(&p, json_string, strlen(json_string), t, JSMN_TOK_MAX);
-    char username[128] = "";
-    char firstName[128] = "";
-    char lastName[128] = "";
-    char publicId[256] = "";
-    char walletId[128] = "";
-    char userId[128] = "";
+    char username[STR_SIZE] = "";
+    char firstName[STR_SIZE] = "";
+    char lastName[STR_SIZE] = "";
+    char publicId[PUBLIC_ID_SIZE] = "";
+    char walletId[STR_SIZE] = "";
+    char userId[STR_SIZE] = "";
     char dummy = '\0';
     for (int i = 0; i < r; i++){
-        if (t[i].size == 0) continue;
-        if (strncmp(&json_string[t[i].start], "username", 8) == 0) {
+        if (t[i].size == 0) {
+            continue;
+        }
+        if (strncmp(&json_string[t[i].start], "username", strlen("username")) == 0) {
             strncpy(username, &json_string[t[i+1].start], t[i+1].end - t[i+1].start);
-        } else if (strncmp(&json_string[t[i].start], "firstName", 9) == 0) {
+        } else if (strncmp(&json_string[t[i].start], "firstName", strlen("firstName")) == 0) {
             strncpy(firstName, &json_string[t[i+1].start], t[i+1].end - t[i+1].start);
-        } else if (strncmp(&json_string[t[i].start], "lastName", 8) == 0) {
+        } else if (strncmp(&json_string[t[i].start], "lastName", strlen("lastName")) == 0) {
             strncpy(lastName, &json_string[t[i+1].start], t[i+1].end - t[i+1].start);
-        } else if (strncmp(&json_string[t[i].start], "userId", 6) == 0) {
+        } else if (strncmp(&json_string[t[i].start], "userId", strlen("userId")) == 0) {
             strncpy(userId, &json_string[t[i+1].start], t[i+1].end - t[i+1].start);
         }
     }
 
     // walletId
-    char url[1024];
+    char url[URL_LEN];
     CURLcode res;
-    snprintf(url, 1024, "%s:%d/account", bc_hostname, bc_port);
+    snprintf(url, URL_LEN, "%s:%d/account", bc_hostname, bc_port);
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, wallet_id_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &walletId);
@@ -313,8 +331,7 @@ void UserManagement_put_string(const char* json_string, char* json_response)
     int i = 0;
     for (i=0; i<strlen(userId); i++) {
         if ((i == 0 && userId[i] == '0' && userId[i+1] == 'x') &&
-            (i == 1 && userId[i] == 'x' && userId[i-1] == '0'))
-        {
+            (i == 1 && userId[i] == 'x' && userId[i-1] == '0')) {
             continue;
         }
         int idx_o = i*2;
@@ -324,31 +341,30 @@ void UserManagement_put_string(const char* json_string, char* json_response)
 
     int rc = insert_user(username, firstName, lastName, walletId, publicId, userId);
     // use url for no particular reason
-    snprintf(url, 1024, "{\"username\": \"%s\", \"firstName\": \"%s\", \"lastName\": \"%s\", \"walletId\": \"%s\", \"publicId\": \"%s\", \"userId\": \"%s\", \"deviceId\": \"%s\"}", username, firstName, lastName, walletId, publicId, userId, device_id);
+    snprintf(url, URL_LEN, "{\"username\": \"%s\", \"firstName\": \"%s\", \"lastName\": \"%s\", \"walletId\": \"%s\", \"publicId\": \"%s\", \"userId\": \"%s\", \"deviceId\": \"%s\"}", username, firstName, lastName, walletId, publicId, userId, device_id);
     fjson_object* fjobj_retdata = fjson_tokener_parse(url);
     fjson_object* fjobj_resp = fjson_object_new_object();
     if (rc == 0) {
         create_json_response(fjobj_resp, fjobj_retdata, 0, "");
     } else {
-        char msg[256] = "";
-        snprintf(msg, 256, "Couldn't insert user '%s'!", username);
+        char msg[MSG_LEN] = "";
+        snprintf(msg, MSG_LEN, "Couldn't insert user '%s'!", username);
         create_json_response(fjobj_resp, NULL, 1, msg);
     }
-    strncpy(json_response, fjson_object_to_json_string(fjobj_resp), 2048);
+    strncpy(json_response, fjson_object_to_json_string(fjobj_resp), QUERY_STR_MAX);
     fjson_object_put(fjobj_resp);
 }
 
-void UserManagement_get_authenteq_id(const char* username, char* json_string)
-{
+void UserManagement_get_authenteq_id(const char* username, char* json_string) {
     fjson_object* fjobj_response = fjson_object_new_object();
-    snprintf(query_string, 2048, "SELECT userId FROM users WHERE username = '%s'", username);
+    snprintf(query_string, QUERY_STR_MAX, "SELECT userId FROM users WHERE username = '%s'", username);
     int rc = query_helper(query_string, get_cb, fjobj_response);
 
     struct fjson_object_iterator iter = fjson_object_iter_begin(fjobj_response);
     struct fjson_object_iterator iter_end = fjson_object_iter_end(fjobj_response);
     int found_user = 0;
     while (!fjson_object_iter_equal(&iter, &iter_end)) {
-        if (strncmp(fjson_object_iter_peek_name(&iter), "userId", 6) == 0) {
+        if (strncmp(fjson_object_iter_peek_name(&iter), "userId", strlen("userId")) == 0) {
             found_user = 1;
             break;
         }
@@ -358,21 +374,20 @@ void UserManagement_get_authenteq_id(const char* username, char* json_string)
     if (rc == 0 && found_user == 1) {
         create_json_response(fjobj_resp, fjobj_response, 0, "");
     } else {
-        char msg[256] = "";
-        snprintf(msg, 256, "Couldn't find user '%s'!", username);
+        char msg[MSG_LEN] = "";
+        snprintf(msg, MSG_LEN, "Couldn't find user '%s'!", username);
         create_json_response(fjobj_resp, NULL, 1, msg);
     }
 
-    strncpy(json_string, fjson_object_to_json_string(fjobj_resp), 2048);
+    strncpy(json_string, fjson_object_to_json_string(fjobj_resp), QUERY_STR_MAX);
 }
 
-void UserManagement_clear_all_users(char* response)
-{
+void UserManagement_clear_all_users(char* response) {
     int count = 0;
     int rc = query_helper("DROP TABLE users", count_cb, &count);
     UserManagement_deinit();
     UserManagement_init(NULL, 0, NULL);
     fjson_object* fjobj_resp = fjson_object_new_object();
     create_json_response(fjobj_resp, NULL, 0, "");
-    strncpy(response, fjson_object_to_json_string(fjobj_resp), 2048);
+    strncpy(response, fjson_object_to_json_string(fjobj_resp), QUERY_STR_MAX);
 }
