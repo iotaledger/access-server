1be7df0c105b50b36c63c64642d44b46a55a8ecc
Golic, Strahinja <strahinja.golic.bp@nttdata.ro>
2020-05-26 19:44:27 +0200

    Storage plugin refactor
    + Refactore of the storage module
    + Implementation of interface for storage on Raspberry Pi.

---
+++ plugins/storage/platforms/r_pi/rpi_storage.c
@@ -0,0 +1,199 @@
+/*
+ * This file is part of the IOTA Access Distribution
+ * (https://github.com/iotaledger/access)
+ *
+ * Copyright (c) 2020 IOTA Stiftung
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/****************************************************************************
+ * \project Decentralized Access Control
+ * \file rpi_storage.c
+ * \brief
+ * Implementation of policy storage API for Raspberry Pi 
+ *
+ * @Author Strahinja Golic
+ *
+ * \notes
+ *
+ * \history
+ * 25.05.2020. Initial version.
+ ****************************************************************************/
+/****************************************************************************
+ * INCLUDES
+ ****************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include "rpi_storage.h"
+
+/****************************************************************************
+ * MACROS
+ ****************************************************************************/
+#define RPI_MAX_STR_LEN 512
+#define RPI_ACCESS_ERR -1
+
+/****************************************************************************
+ * API FUNCTIONS
+ ****************************************************************************/
+bool RPI_store_policy(char* policy_id, char* policy_object, int policy_object_size,
+						char* signature, char* public_key, char* signature_algorithm,
+						char* hash_function)
+{
+	char pol_path[RPI_MAX_STR_LEN] = {0};
+	FILE *f;
+
+	//Check input parameters
+	if ((policy_id == NULL) || (policy_object == NULL) || (policy_object_size = 0) || (signature == NULL) ||
+		(public_key == NULL) || (signature_algorithm == NULL) || (hash_function == NULL))
+	{
+		printf("\nERROR[%s]: Bad input parameter.\n", __FUNCTION__);
+		return FALSE;
+	}
+
+	//Write policy data to a file
+	sprintf(pol_path, "../plugins/storage/platforms/r_pi/policies/%s.txt", policy_id);
+	f = fopen(pol_path, "w+");
+
+	fwrite("policy id:", strlen("policy id:"), 1, f);
+	fwrite(policy_id, strlen(policy_id), 1, f);
+	fwrite("\npolicy object:", strlen("\npolicy object:"), 1, f);
+	fwrite(policy_object, policy_object_size, 1, f);
+	fwrite("\npolicy id signature:", strlen("\npolicy id signature:"), 1, f);
+	fwrite(signature, strlen(signature), 1, f);
+	fwrite("\npolicy id signature public key:", strlen("\npolicy id signature public key:"), 1, f);
+	fwrite(public_key, strlen(public_key), 1, f);
+	fwrite("\npolicy id signature sign. algorithm:", strlen("\npolicy id signature sign. algorithm:"), 1, f);
+	fwrite(signature_algorithm, strlen(signature_algorithm), 1, f);
+	fwrite("\nhash function:", strlen("\nhash function:"), 1, f);
+	fwrite(hash_function, strlen(hash_function), 1, f);
+
+	fclose(f);
+
+	return TRUE;
+}
+
+bool RPI_acquire_policy(char* policy_id, char* policy_object, int *policy_object_size,
+						char* signature, char* public_key, char* signature_algorithm,
+						char* hash_function)
+{
+	char pol_path[RPI_MAX_STR_LEN] = {0};
+	char *buffer;
+	char *substr;
+	int buff_len;
+	int substr_len;
+	FILE *f;
+
+	//Check input parameters
+	if ((policy_id == NULL) || (policy_object == NULL) || (policy_object_size = NULL) || (signature == NULL) ||
+		(public_key == NULL) || (signature_algorithm == NULL) || (hash_function == NULL))
+	{
+		printf("\nERROR[%s]: Bad input parameter.\n", __FUNCTION__);
+		return FALSE;
+	}
+
+	//Open file
+	sprintf(pol_path, "../plugins/storage/platforms/r_pi/policies/%s.txt", policy_id);
+	f = fopen(pol_path, "r");
+
+	//Get file size
+	fseek(f, 0L, SEEK_END);
+	buff_len = ftell(f);
+	fseek(f, 0L, SEEK_SET);
+
+	//Write policy to a buffer
+	buffer = malloc(buff_len * sizeof(char));
+
+	fread(buffer, buff_len * sizeof(char), 1, f);
+	fclose(f);
+
+	//Set the arguments
+	substr = strstr(buffer, "policy object:");
+	substr_len = strstr(buffer, "\npolicy id signature:") - (substr + strlen("policy object:"));
+	memcpy(policy_object, substr + strlen("policy object:"), substr_len);
+
+	*policy_object_size = substr_len;
+
+	substr = strstr(buffer, "policy id signature:");
+	substr_len = strstr(buffer, "\npolicy id signature public key:") - (substr + strlen("policy id signature:"));
+	memcpy(signature, substr + strlen("policy id signature:"), substr_len);
+
+	substr = strstr(buffer, "policy id signature public key:");
+	substr_len = strstr(buffer, "\npolicy id signature sign. algorithm:") - (substr + strlen("policy id signature public key:"));
+	memcpy(public_key, substr + strlen("policy id signature public key:"), substr_len);
+
+	substr = strstr(buffer, "policy id signature sign. algorithm:");
+	substr_len = strstr(buffer, "\nhash function:") - (substr + strlen("policy id signature sign. algorithm:"));
+	memcpy(signature_algorithm, substr + strlen("policy id signature sign. algorithm:"), substr_len);
+
+	substr = strstr(buffer, "hash function:");
+	substr_len = &buffer[buff_len] - (substr + strlen("hash function:"));
+	memcpy(hash_function, substr + strlen("hash function:"), substr_len);
+
+	free(buffer);
+
+	return TRUE;
+}
+
+bool RPI_check_if_stored_policy(char* policy_id)
+{
+	char pol_path[RPI_MAX_STR_LEN] = {0};
+
+	//Check input parameters
+	if (policy_id == NULL)
+	{
+		printf("\nERROR[%s]: Bad input parameter.\n", __FUNCTION__);
+		return FALSE;
+	}
+
+	sprintf(pol_path, "../plugins/storage/platforms/r_pi/policies/%s.txt", policy_id);
+
+	//Check file existance
+	if(access(pol_path, F_OK) != RPI_ACCESS_ERR)
+	{
+		//File exists
+		return TRUE;
+	}
+	else
+	{
+		//File doesn't exist
+		return FALSE;
+	}
+}
+
+bool RPI_flush_policy(char* policy_id)
+{
+	char pol_path[RPI_MAX_STR_LEN] = {0};
+
+	//Check input parameters
+	if (policy_id == NULL)
+	{
+		printf("\nERROR[%s]: Bad input parameter.\n", __FUNCTION__);
+		return FALSE;
+	}
+
+	sprintf(pol_path, "../plugins/storage/platforms/r_pi/policies/%s.txt", policy_id);
+
+	if (remove(pol_path) == 0)
+	{
+		//Success
+		return TRUE;
+	}
+	else
+	{
+		//Fail
+		return FALSE;
+	}
+}
--- plugins/storage/storage.h
+++ plugins/storage/storage.h
@@ -18,250 +18,71 @@
  */
 
 /****************************************************************************
- * \project Policy Store
- * \file policystore.h
+ * \project Decentralized Access Control
+ * \file storage.h
  * \brief
- * Implementation of policy store
+ * Implementation of policy storage interface
  *
- * @Author Dejan Nedic
+ * @Author Dejan Nedic, Strahinja Golic
  *
  * \notes
  *
  * \history
  * 24.08.2018. Initial version.
+ * 25.05.2020. Refactoring.
  ****************************************************************************/
+#ifndef _STORAGE_H_
+#define _STORAGE_H_
 
-#ifndef _POLICYSOTRE_H_
-#define _POLICYSOTRE_H_
+/****************************************************************************
+ * MACROS
+ ****************************************************************************/
+#ifndef bool
+#define bool _Bool
+#endif
+#ifndef TRUE
+#define TRUE 1
+#endif
+#ifndef FALSE
+#define FALSE 0
+#endif
 
-#include <stdio.h>
+//Set R-Pi as used platform
+#define USE_RPI 1
 
-#define HASH_L (32)
+/****************************************************************************
+ * TYPES
+ ****************************************************************************/
+typedef enum
+{
+    STORAGE_OK,
+    STORAGE_ERROR,
+} STORAGE_error_t;
 
-typedef struct policy {
-    short policy_ID_size;
-    int policy_size_uncompressed;
-    int policy_size_compressed;
-    short policy_cost_size;
-    int is_paid;
-    int num_of_executions;
-
-    char *policy_cost;
-    char *policy_id;
-    char *policy_c;
-} policy_t;
-
-typedef enum {
-    OK,
-    ERROR,
-} error_t;
-
-typedef struct node {
-    policy_t *pol;
-    struct node * next;
-} node_t;
-
-typedef struct list {
-    char *policyID;
-    char *action;
-    int action_length;
-
-    char *policy_cost;
-    int policy_cost_size;
-
-    struct list *next;
-} list_t;
-
-typedef struct policy_id_signature {
-    int signature_algorithm_size;
-    char *signature_algorithm;
-    int signature_size;
-    char *signature;
-    int public_key_size;
-    char *public_key;
-} policy_id_signature_t;
+/****************************************************************************
+ * API FUNCTIONS
+ ****************************************************************************/
+/**
+ * @fn      Storage_init
+ *
+ * @brief   Initialize storage module
+ * 
+ * @param   void
+ *
+ * @return  STORAGE_error_t state.
+ */
+STORAGE_error_t Storage_init(void);
 
 /**
- * @fn 		void PolicyStore_free_policy(policy_t *pol)
+ * @fn      Storage_term
  *
- * @brief 	Function that frees memory allocated for policy_t structure
- * 			and its fields.
+ * @brief   Terminate storage module
+ * 
+ * @param   void
  *
- * @param	pol Pointer to policy structure
+ * @return  STORAGE_error_t state.
  */
-void PolicyStore_free_policy(policy_t *pol);
+STORAGE_error_t Storage_term(void);
 
-/**
- * @fn 		free_ps_list_item(list_t *item)
- *
- * @brief	Function that frees memoty allocated for structure
- * 			representing action item and its fields.
- * 			Does not frees memory for pointer representing "next"
- * 			element.
- *
- * @param	item Pointer to action
- */
-void PolicyStore_free_action_list_item(list_t *item);
 
-/**
- * @fn      int PolicyStore_init( )
- *
- * @brief   Function that initiates policy store, puts three policies in the policy store
- *
- * @return  0 if it succeeds, 1 if it fails.
- */
-int PolicyStore_init( );
-
-/**
- * @fn      int PolicyStore_put_policy(char *policy_id, int policy_id_size, char *policy, int policy_size,
- *                                     char *policy_id_signature, int policy_id_signature_size, char *policy_cost, short policy_cost_sizechar *policy_id, int policy_id_size, char *policy, int policy_size)
- *
- * @brief   Function that receives policy from cloud and puts new policy to the policy store
- *
- * @param   policy_id                policy ID
- * @param   policy_id_size           policy id size
- * @param   signed_policy            signed policy
- * @param   signed_policy_size       signed policy size
- * @param   policy_id_signature      policy id signature
- * @param   policy_id_signature_size policy id signature size
- * @param   policy_cost              policy cost
- * @param   policy_cost_size         policy cost size
- *
- * @return  0 if it succeeds, 1 if the policy is already located in the policy store, -1 for error.
- */
-int PolicyStore_put_policy(char *policy_id, int policy_id_size, char *signed_policy, int signed_policy_size,
-                           char *policy_id_signature, int policy_id_signature_size, char *policy_cost, short policy_cost_size);
-
-/**
- * @fn      int PolicyStore_put_string(char *pol_val, char *sig_val)
- *
- * @brief   Function that checks policy store for policy with id equal to policyID
- *
- * @param   pol_val        Policy ID
- * @param   sig_val        Policy ID size
- *
- * @return  0 if it succeeds, 1 if it fails.
- */
-int PolicyStore_put_string(char *pol_val, char *sig_val, char *policy_cost, int policy_cost_size); // TODO add parameters to documentation
-
-/**
- * @fn      int PolicyStore_get_string(char **pol_string, char *psID, short policyIDSize)
- *
- * @brief   Function that searches policy store for policy with the id equal to policyID and returns policy if it exists
- *
- * @param   pol_string      Pointer to memory where requested policy is stored
- * @param   psID            Policy ID
- * @param   policyIDSize    Policy ID size
- *
- * @return  0 if it succeeds, 1 if it fails.
- */
-int PolicyStore_get_string(char **pol_string, char *psID, short policyIDSize);
-
-/**
- * @fn      int PolicyStore_has_string(char *policyID, short policyIDSize)
- *
- * @brief   Function that checks policy store for policy with id equal to policyID
- *
- * @param   policyID        Policy ID
- * @param   policyIDSize    Policy ID size
- *
- * @return  1 if it succeeds, 0 if it fails.
- */
-int PolicyStore_has_string(char *policyID, short policyIDSize);
-
-/**
- * @fn      int PolicyStore_del_policy(char *policyID, int policyIDSize)
- *
- * @brief   Function that deletes policy from the policy store if the policy exists
- *
- * @param   policyID        ID of the requested policy
- * @param   policyIDSize    Size of policyID
- *
- * @return  0 if it succeeds, 1 if it policy doesn't exist.
- */
-int PolicyStore_del_policy(char *policyID, int policyIDSize);
-
-/**
- * @fn      int PolicyStore_get_list_of_actions(char *subject_id, int subject_id_length, list_t **action_list)
- *
- * @brief   Function that checks policy store for policy that contains subject id
- *
- * @param   subject_id        	subject id
- * @param   subject_id_length	subject id length
- * @param	action_list			list of requested action
- *
- * @return  number of policies that contain subject id.
- */
-int PolicyStore_get_list_of_actions(char *subject_id, int subject_id_length, list_t **action_list);
-
-/**
- * @fn      int PolicyStore_clean()
- *
- * @brief   Function that deletes all the policies from the policy store, except first three
- *
- * @return  0.
- */
-int PolicyStore_clean();
-
-/**
- * @fn      int PolicyStore_clear_list_of_actions(list_t **action_list)
- *
- * @brief   Function that frees allocated memory for the list of actions
- *
- * @param	action_list list of actions
- *
- * @return  0.
- */
-int PolicyStore_clear_list_of_actions(list_t *action_list);
-
-/**
- * @fn      int PolicyStore_ps_aws_sync(char *AWS_list, int number_of_policies)
- *
- * @brief   Function that synchronizes local policy store with AWS version.
- * 			Deletes all policies it can not find on AWS.
- *
- * @param	AWS_list 				AWS list of policies
- * @param	number_od_policies		number of policies in AWS policy store
- *
- * @return  0.
- */
-int PolicyStore_ps_aws_sync(char *AWS_list, int number_of_policies);
-
- /**
-  * @fn 	int PolicyStore_is_policy_paid(char *policy_id)
-  *
-  * @brief 	Function that checks if policy is paid or not
-  *
-  * @param 	policy_id 		policy ID
-  * @param 	policy_id_size 	policy ID size
-  *
-  * @return 0 if policy is NOT paid, 1 if policy IS paid
-  */
-int PolicyStore_is_policy_paid(char *policy_id, int policy_id_size);
-
-/**
- * @fn		int PolicyStore_enable_policy(char *policy_id, int policy_id_size)
- *
- * @brief	Function that enables policy. Policy is disabled if it
- * 			is not paid.
- *
- * @param	policy_id		policy ID
- * @param 	policy_id_size	policy ID size
- *
- * @return 	0 if policy is successfully enabled, 1 otherwise
- */
-int PolicyStore_enable_policy(char *policy_id, int policy_id_size);
-
-/**
- * @fn		policy_t *PolicyStore_get_policy(char *policy_id, int policy_id_size)
- *
- * @brief	Function that gets policy.
- *
- * @param	policy_id	policy ID
- * @param 	policy_id_size	policy ID size
- *
- * @return 	Required policy
- */
-policy_t *PolicyStore_get_policy(char *policy_id, int policy_id_size);
-
-#endif // _POLICYSOTRE_H_
+#endif //_STORAGE_H_
