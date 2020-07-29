d5ba03d5783e04d6df5ccce9e5706230993baa0f
bernardo <bernardoaraujor@gmail.com>
2020-07-29 15:01:01 +0200

    rename unix to posix

--- portability/os/posix/posix_transaction.h
+++ portability/os/posix/posix_transaction.h
@@ -19,9 +19,9 @@
 
 /****************************************************************************
  * \project IOTA Access
- * \file unix_trans.h
+ * \file posix_trans.h
  * \brief
- * Implementation of API for transactions storage to unix
+ * Implementation of API for transactions storage to posix
  *
  * @Author Strahinja Golic, Bernardo Araujo.
  *
@@ -29,10 +29,10 @@
  *
  * \history
  * 10.06.2020. Initial version.
- * 28.07.2020. Renaming.
+ * 29.07.2020. Renaming.
  ****************************************************************************/
-#ifndef _UNIX_TRANSACTION_H_
-#define _unix_TRANSACTION_H_
+#ifndef _POSIX_TRANSACTION_H_
+#define _POSIX_TRANSACTION_H_
 
 /****************************************************************************
  * MACROS
@@ -51,21 +51,21 @@
  * API FUNCTIONS
  ****************************************************************************/
 /**
- * @fn      unix_transaction_store
+ * @fn      posix_transaction_store
  *
- * @brief   Store trnasaction on unix
+ * @brief   Store trnasaction on posix
  *
  * @param   policy_id - Policy ID string
  * @param   policy_id_len - Length of policy ID string
  *
  * @return  TRUE - success, FALSE - fail
  */
-bool unix_transaction_store(char* policy_id, int policy_id_len);
+bool posix_transaction_store(char* policy_id, int policy_id_len);
 
 /**
- * @fn      unix_transaction_store
+ * @fn      posix_transaction_store
  *
- * @brief   Store transaction on unix
+ * @brief   Store transaction on posix
  *
  * @param   policy_id - Policy ID string
  * @param   policy_id_len - Length of policy ID string
@@ -73,21 +73,21 @@ bool unix_transaction_store(char* policy_id, int policy_id_len);
  *
  * @return  TRUE - success, FALSE - fail
  */
-bool unix_transaction_update_payment_status(char* policy_id, int policy_id_len, bool is_verified);
+bool posix_transaction_update_payment_status(char* policy_id, int policy_id_len, bool is_verified);
 
 /**
- * @fn      unix_transaction_is_stored
+ * @fn      posix_transaction_is_stored
  *
- * @brief   Check if transaction is stored on unix
+ * @brief   Check if transaction is stored on posix
  *
  * @param   policy_id - Policy ID string
  *
  * @return  TRUE - stored, FALSE - not stored
  */
-bool unix_transaction_is_stored(char* policy_id);
+bool posix_transaction_is_stored(char* policy_id);
 
 /**
- * @fn      unix_transaction_is_verified
+ * @fn      posix_transaction_is_verified
  *
  * @brief   Check if transaction payment is verified
  *
@@ -96,6 +96,6 @@ bool unix_transaction_is_stored(char* policy_id);
  *
  * @return  TRUE - verified, FALSE - not verified
  */
-bool unix_transaction_is_verified(char* policy_id, int policy_id_len);
+bool posix_transaction_is_verified(char* policy_id, int policy_id_len);
 
-#endif  //_unix_TRANSACTION_H_
+#endif  //_POSIX_TRANSACTION_H_
